033586511e03619e8aae25b60c4e405af2210712
Isaac <uniqucode@gmail.com>
2023-03-09 14:10:53 +0100

    Update URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -129,7 +129,9 @@ const App: React.FC = () => {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider(
+            "https://mern-dashboard-l2fl.onrender.com/api/v1"
+          )}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
